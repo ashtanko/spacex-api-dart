973618674f3e38b0f4da64958462344711e902a5
Oleksii Shtanko <shtankopro@gmail.com>
2022-11-04 10:54:13 +0200

    [WIP] Resolve dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc_app_template/app.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:spacex_api_dart/app.dart';
 
 Future<void> main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
