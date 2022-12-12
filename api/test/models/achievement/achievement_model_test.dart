a4878133c5cb19d4665ff98108906e2557db6410
Oleksii Shtanko <shtankopro@gmail.com>
2022-12-12 17:22:24 +0200

    Refactor achievement model tests

--- api/test/models/achievement/achievement_model_test.dart
+++ api/test/models/achievement/achievement_model_test.dart
@@ -1,11 +1,44 @@
+import 'dart:convert';
+
 import 'package:api/models/index.dart';
 import 'package:test/test.dart';
 
+import '../../json_reader.dart';
+
 void main() {
-  group('AchievementModel', () {
-    test('supports value comparison', () {
+  final achievementModel = AchievementModel(
+    links: const AchievementLinksModel(
+      article: 'http://www.spacex.com/news/2013/02/11/flight-4-launch-update-0',
+    ),
+    name: 'Falcon reaches Earth orbit',
+    date: DateTime.parse('2008-09-28T23:15:00Z'),
+    details:
+        'Falcon 1 becomes the first privately developed liquid-fuel rocket to reach Earth orbit.',
+    id: '5f6fb2cfdcfdf403df37971e',
+  );
+
+  group('value comparison', () {
+    test('should be equal', () {
       expect(const AchievementModel(id: '0'), const AchievementModel(id: '0'));
     });
+  });
+
+  group('from json', () {
+    test(
+      'should return a valid model from json',
+      () async {
+        // arrange
+        final Map<String, dynamic> jsonMap = json.decode(
+          readJson('fixtures/achievement.json'),
+        ) as Map<String, dynamic>;
+
+        // act
+        final result = AchievementModel.fromJson(jsonMap);
+
+        // assert
+        expect(result, equals(achievementModel));
+      },
+    );
 
     test('is correctly generated from a JSON', () {
       expect(
@@ -21,20 +54,36 @@ void main() {
               'Falcon 1 becomes the first privately developed liquid-fuel rocket to reach Earth orbit.',
           'id': '5f6fb2cfdcfdf403df37971e'
         }),
-        AchievementModel(
-          links: const AchievementLinksModel(
-            article:
-                'http://www.spacex.com/news/2013/02/11/flight-4-launch-update-0',
-          ),
-          name: 'Falcon reaches Earth orbit',
-          date: DateTime.parse('2008-09-28T23:15:00Z'),
-          details:
-              'Falcon 1 becomes the first privately developed liquid-fuel rocket to reach Earth orbit.',
-          id: '5f6fb2cfdcfdf403df37971e',
-        ),
+        achievementModel,
       );
     });
+  });
 
+  group('to json', () {
+    test(
+      'should return a json map containing proper data',
+      () async {
+        // act
+        final result = achievementModel.toJson();
+
+        // assert
+        const expectedJsonMap = {
+          'id': '5f6fb2cfdcfdf403df37971e',
+          'title': 'Falcon reaches Earth orbit',
+          'details':
+              'Falcon 1 becomes the first privately developed liquid-fuel rocket to reach Earth orbit.',
+          'links': {
+            'article':
+                'http://www.spacex.com/news/2013/02/11/flight-4-launch-update-0'
+          },
+          'event_date_utc': '2008-09-28T23:15:00.000Z'
+        };
+        expect(result, equals(expectedJsonMap));
+      },
+    );
+  });
+
+  group('parse date', () {
     test('correctly returns parsed date', () {
       expect(
         AchievementModel(date: DateTime.parse('2008-09-28T23:15:00Z'), id: '')
