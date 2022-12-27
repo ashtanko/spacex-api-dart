fdbf438c602bcd96f5e0f5ad01c2fc00a82722d0
Oleksii Shtanko <shtankopro@gmail.com>
2022-12-27 11:29:23 +0200

    Add VehicleApi unit tests

--- api/lib/network/vehicle_data_source.dart
+++ api/lib/network/vehicle_data_source.dart
@@ -2,6 +2,8 @@ import 'package:api/api.dart';
 import 'package:api/api/vehicle/vehicle_api.dart';
 import 'package:api/models/query/query.dart' as q;
 import 'package:api/models/response/api_paginated_list.dart';
+import 'package:api/utils/exception.dart';
+import 'package:dio/dio.dart';
 
 class VehicleDataSource {
   VehicleDataSource(this._api);
@@ -9,52 +11,98 @@ class VehicleDataSource {
   final VehicleApi _api;
 
   Future<List<DragonVehicleModel>> getAllDragons() async {
-    return _api.getAllDragons();
+    try {
+      return _api.getAllDragons();
+    } on DioError catch (_) {
+      throw ServerException();
+    }
   }
 
   Future<DragonVehicleModel> getDragon(String id) async {
-    return _api.getDragon(id);
+    try {
+      return _api.getDragon(id);
+    } on DioError catch (_) {
+      throw ServerException();
+    }
   }
 
   Future<ApiPaginatedList<DragonVehicleModel>> queryDragons(
     q.Query query,
   ) async {
-    return _api.queryDragons(query);
+    try {
+      return _api.queryDragons(query);
+    } on DioError catch (_) {
+      throw ServerException();
+    }
   }
 
   // ships
   Future<List<ShipVehicleModel>> getAllShips() async {
-    return _api.getAllShips();
+    try {
+      return _api.getAllShips();
+    } on DioError catch (_) {
+      throw ServerException();
+    }
   }
 
   Future<ShipVehicleModel> getShip(String id) async {
-    return _api.getShip(id);
+    try {
+      return _api.getShip(id);
+    } on DioError catch (_) {
+      throw ServerException();
+    }
   }
 
   Future<ApiPaginatedList<ShipVehicleModel>> queryShips(q.Query query) async {
-    return _api.queryShips(query);
+    try {
+      return _api.queryShips(query);
+    } on DioError catch (_) {
+      throw ServerException();
+    }
   }
 
   Future<ApiPaginatedList<ShipFullVehicleModel>> queryFullShips(
     q.Query query,
   ) async {
-    return _api.queryFullShips(query);
+    try {
+      return _api.queryFullShips(query);
+    } on DioError catch (_) {
+      throw ServerException();
+    }
   }
 
   // rockets
   Future<List<RocketVehicleModel>> getAllRockets() async {
-    return _api.getAllRockets();
+    try {
+      return _api.getAllRockets();
+    } on DioError catch (_) {
+      throw ServerException();
+    }
   }
 
   Future<RocketVehicleModel> getRocket(String id) async {
-    return _api.getRocket(id);
+    try {
+      return _api.getRocket(id);
+    } on DioError catch (_) {
+      throw ServerException();
+    }
   }
 
-  Future<ApiPaginatedList<RocketVehicleModel>> queryRockets(q.Query query) {
-    return _api.queryRockets(query);
+  Future<ApiPaginatedList<RocketVehicleModel>> queryRockets(
+    q.Query query,
+  ) async {
+    try {
+      return _api.queryRockets(query);
+    } on DioError catch (_) {
+      throw ServerException();
+    }
   }
 
-  Future<RoadsterVehicleModel> getRoadster() {
-    return _api.getRoadster();
+  Future<RoadsterVehicleModel> getRoadster() async {
+    try {
+      return _api.getRoadster();
+    } on DioError catch (_) {
+      throw ServerException();
+    }
   }
 }
