716f0a5d37deb664248cb7552bd21d82b88c6fad
Oleksii Shtanko <shtankopro@gmail.com>
2022-12-27 10:23:59 +0200

    Add LaunchpadsApi unit tests

--- api/lib/network/launchpads_data_source.dart
+++ api/lib/network/launchpads_data_source.dart
@@ -2,6 +2,8 @@ import 'package:api/api/launchpads/launchpads_api.dart';
 import 'package:api/models/launchpad/launchpad_model.dart';
 import 'package:api/models/query/query.dart' as q;
 import 'package:api/models/response/api_paginated_list.dart';
+import 'package:api/utils/exception.dart';
+import 'package:dio/dio.dart';
 
 class LaunchpadsDataSource {
   LaunchpadsDataSource(this._api);
@@ -9,22 +11,38 @@ class LaunchpadsDataSource {
   final LaunchpadsApi _api;
 
   Future<List<LaunchpadModel>> getAllLaunchpads() async {
-    return _api.getAllLaunchpads();
+    try {
+      return _api.getAllLaunchpads();
+    } on DioError catch (_) {
+      throw ServerException();
+    }
   }
 
   Future<LaunchpadModel> getLaunchpad(String id) async {
-    return _api.getLaunchpad(id);
+    try {
+      return _api.getLaunchpad(id);
+    } on DioError catch (_) {
+      throw ServerException();
+    }
   }
 
   Future<ApiPaginatedList<LaunchpadModel>> queryLaunchpads(
     q.Query query,
   ) async {
-    return _api.queryLaunchpads(query);
+    try {
+      return _api.queryLaunchpads(query);
+    } on DioError catch (_) {
+      throw ServerException();
+    }
   }
 
   Future<ApiPaginatedList<LaunchpadFullModel>> queryFullLaunchpads(
     q.Query query,
   ) async {
-    return _api.queryFullLaunchpads(query);
+    try {
+      return _api.queryFullLaunchpads(query);
+    } on DioError catch (_) {
+      throw ServerException();
+    }
   }
 }
---
+++ api/test/api/launchpads/launchpads_api_test.dart
@@ -0,0 +1,198 @@
+import 'package:api/api/launchpads/launchpads_api.dart';
+import 'package:api/models/launchpad/launchpad_model.dart';
+import 'package:api/models/query/query.dart';
+import 'package:api/models/response/api_paginated_list.dart';
+import 'package:api/network/launchpads_data_source.dart';
+import 'package:api/utils/exception.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+import '../../fixtures_reader.dart';
+import '../../helpers/mock_dio_exception.dart';
+
+class MockLaunchpadsApi extends Mock implements LaunchpadsApi {}
+
+void main() {
+  late MockLaunchpadsApi api;
+  late LaunchpadsDataSource dataSource;
+
+  setUp(() async {
+    registerFallbackValue(Uri());
+    api = MockLaunchpadsApi();
+    dataSource = LaunchpadsDataSource(api);
+  });
+
+  group('getAllLaunchpads', () {
+    final rawJson = 'launchpads/launchpad.json'.toFixture();
+    final model = LaunchpadModel.fromJson(rawJson);
+    final mockResponse = [model];
+    test(
+      'should perform a GET request on /launchpads',
+      () async {
+        // arrange
+        when(
+          () => api.getAllLaunchpads(),
+        ).thenAnswer(
+          (_) async => mockResponse,
+        );
+
+        // act
+        dataSource.getAllLaunchpads();
+        // assert
+        verify(() => api.getAllLaunchpads());
+        verifyNoMoreInteractions(api);
+      },
+    );
+  });
+
+  group('getLaunch', () {
+    const id = '5e9e4501f5090910d4566f83';
+    final rawJson = 'launchpads/launchpad.json'.toFixture();
+    final mockResponse = LaunchpadModel.fromJson(rawJson);
+    test(
+      'should perform a GET request on /launchpads/{$id}',
+      () async {
+        // arrange
+        when(
+          () => api.getLaunchpad(id),
+        ).thenAnswer(
+          (_) async => mockResponse,
+        );
+
+        // act
+        dataSource.getLaunchpad(id);
+        // assert
+        verify(() => api.getLaunchpad(id));
+        verifyNoMoreInteractions(api);
+      },
+    );
+  });
+
+  group('queryLaunchpads', () {
+    final rawJson = 'launchpads/launchpad.json'.toFixture();
+    final mockResponse = ApiPaginatedList(
+      results: [
+        LaunchpadModel.fromJson(rawJson),
+      ],
+    );
+    final queryRawJson = 'query/query.json'.toFixture();
+    final query = Query.fromJson(queryRawJson);
+    test(
+      'should perform a POST request on /launchpads/query',
+      () async {
+        // arrange
+        when(
+          () => api.queryLaunchpads(query),
+        ).thenAnswer(
+          (_) async => mockResponse,
+        );
+
+        // act
+        dataSource.queryLaunchpads(query);
+        // assert
+        verify(() => api.queryLaunchpads(query));
+        verifyNoMoreInteractions(api);
+      },
+    );
+  });
+
+  group('queryFullLaunchpads', () {
+    final rawJson = 'launchpads/full_launchpad.json'.toFixture();
+    final mockResponse = ApiPaginatedList(
+      results: [
+        LaunchpadFullModel.fromJson(rawJson),
+      ],
+    );
+    final queryRawJson = 'query/query.json'.toFixture();
+    final query = Query.fromJson(queryRawJson);
+    test(
+      'should perform a POST request on /launchpads/query',
+      () async {
+        // arrange
+        when(
+          () => api.queryFullLaunchpads(query),
+        ).thenAnswer(
+          (_) async => mockResponse,
+        );
+
+        // act
+        dataSource.queryFullLaunchpads(query);
+        // assert
+        verify(() => api.queryFullLaunchpads(query));
+        verifyNoMoreInteractions(api);
+      },
+    );
+  });
+
+  group('exceptions', () {
+    test(
+      'getAllLaunchpads, should throw a ServerException when the response code is 404 or other (unsuccessful)',
+      () async {
+        // arrange
+        when(() => api.getAllLaunchpads()).thenThrow(
+          dioException,
+        );
+        // act
+        final call = dataSource.getAllLaunchpads();
+        // assert
+        expect(
+          () => call,
+          throwsA(const TypeMatcher<ServerException>()),
+        );
+      },
+    );
+
+    test(
+      'getLaunchpad, should throw a ServerException when the response code is 404 or other (unsuccessful)',
+      () async {
+        // arrange
+        when(() => api.getLaunchpad('id')).thenThrow(
+          dioException,
+        );
+        // act
+        final call = dataSource.getLaunchpad('id');
+        // assert
+        expect(
+          () => call,
+          throwsA(const TypeMatcher<ServerException>()),
+        );
+      },
+    );
+
+    test(
+      'queryLaunchpads, should throw a ServerException when the response code is 404 or other (unsuccessful)',
+      () async {
+        const q = Query();
+        // arrange
+        when(() => api.queryLaunchpads(q)).thenThrow(
+          dioException,
+        );
+        // act
+        final call = dataSource.queryLaunchpads(q);
+        // assert
+        expect(
+          () => call,
+          throwsA(const TypeMatcher<ServerException>()),
+        );
+      },
+    );
+
+    test(
+      'queryFullLaunchpads, should throw a ServerException when the response code is 404 or other (unsuccessful)',
+      () async {
+        const q = Query();
+        // arrange
+        when(() => api.queryFullLaunchpads(q)).thenThrow(
+          dioException,
+        );
+        // act
+        final call = dataSource.queryFullLaunchpads(q);
+        // assert
+        expect(
+          () => call,
+          throwsA(const TypeMatcher<ServerException>()),
+        );
+      },
+    );
+  });
+}
