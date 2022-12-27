1b7c5023681e3626a3a4c44d791c5f54153c75a8
Oleksii Shtanko <shtankopro@gmail.com>
2022-12-27 14:08:56 +0200

    Clean up

--- api/test/mock_helper.dart
+++ api/test/mock_helper.dart
@@ -1,10 +1,7 @@
-import 'package:http/http.dart' as http;
 import 'package:mockito/annotations.dart';
 
 @GenerateMocks(
   [],
-  customMocks: [
-    MockSpec<http.Client>(as: #MockHttpClient),
-  ],
+  customMocks: [],
 )
 void main() {}
