a09dd7b40487b1668d172868156e198dae7db282
Oleksii Shtanko <shtankopro@gmail.com>
2023-01-15 22:23:26 +0200

    Small fixes

--- lib/data.dart
+++ lib/data.dart
@@ -1,6 +1,7 @@
 library spacex_api_dart;
 
 export 'src/api/api.dart';
+export 'src/di/data_injectable.dart';
 export 'src/interceptor/pretty_logging_interceptor.dart';
 export 'src/models/models.dart';
 export 'src/network/network.dart';
